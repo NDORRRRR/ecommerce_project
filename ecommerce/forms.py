3ed9863776f84d35bcb1c84f9143fe58853826c7
NDORRRRR <24051204191@mhs.unesa.ac.id>
2025-06-05 20:31:48 +0700

    2.3

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User, Produk, Pengiriman, Transaksi
 from allauth.account.forms import SignupForm
-from allauth.socialaccount.form import SignupForm as SocialSignForm
+from allauth.socialaccount.forms import SignupForm as SocialSignupForm  
 
 class UserRegistrationForm(UserCreationForm):
     nama = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
